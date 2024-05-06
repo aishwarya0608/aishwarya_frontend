e4bffc815ce2b7745b0572ee44cdd7cbcb264bfa
aishwarya0608 <167781832+aishwarya0608@users.noreply.github.com>
2024-05-06 11:58:18 +0530

    Update App.js

--- React/Screens_Task/src/App.js
+++ React/Screens_Task/src/App.js
@@ -7,16 +7,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //import Navb from './Components/Navbars';
 //import Shapes from './Components/Image1';
 
-//import ShapeExample from './Components/Screen1';
-//import BasicExamples from './Components/Screen2';
+import ShapeExample from './Components/Screen1';
+
 import BasicExampless from './Components/Screen3';
+import Screen2 from './Components/Screen2'
 function App() {
   return (
     <div className="App">
-     
-      {/*<ShapeExample/>*/} 
-       {/*<BasicExamples/>*/} 
-       <BasicExampless/>
+
+      
+    <ShapeExample/>
+      <Screen2/>
+           <BasicExampless/>
     </div>
   );
 }
